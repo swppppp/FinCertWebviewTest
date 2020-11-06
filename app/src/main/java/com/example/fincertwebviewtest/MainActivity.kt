adec2b1e9f92667b906011d0c28d4192582c8c88
siwon <siwon9311@gmail.com>
2020-11-06 18:02:34 +0900

    -

--- app/src/main/java/com/example/fincertwebviewtest/MainActivity.kt
+++ app/src/main/java/com/example/fincertwebviewtest/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.fincertwebviewtest
 
 import android.content.SharedPreferences
 import android.log.Log
+import android.os.Build
 import android.os.Bundle
 import android.util.Base64
+import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.fincertwebviewtest.databinding.ActivityMainBinding
+import com.fasterxml.jackson.databind.ObjectMapper
 import java.util.*
+
 
 const val ANDROID_APP = "androidAPP"
 class MainActivity : AppCompatActivity() {
@@ -21,7 +27,92 @@ class MainActivity : AppCompatActivity() {
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 //        setContentView(R.layout.activity_main)
 
+        webView = mainBinding.webview
+        webView.loadUrl()
+
+        // -------------------------------------------------------------
+        // 성능관련 최적화 (Optional)
+        // -------------------------------------------------------------
+        // -------------------------------------------------------------
+        // 성능관련 최적화 (Optional)
+        // -------------------------------------------------------------
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+        }
+        // -------------------------------------------------------------
+        // 웹뷰 네트워크 사용설정
+        // -------------------------------------------------------------
+        // -------------------------------------------------------------
+        // 웹뷰 네트워크 사용설정
+        // -------------------------------------------------------------
+        webView.setNetworkAvailable(true)
+
+        // -------------------------------------------------------------
+        // 웹뷰 클라이언트 설정
+        // -------------------------------------------------------------
+
+        // -------------------------------------------------------------
+        // 웹뷰 클라이언트 설정
+        // -------------------------------------------------------------
+        webView.webViewClient = YesKeyWebView()
+        webView.webChromeClient = YesKeyChromeWebView()
+
+        // -------------------------------------------------------------
+        // 웹뷰 Javascript Interface 생성
+        // -------------------------------------------------------------
+
+        // -------------------------------------------------------------
+        // 웹뷰 Javascript Interface 생성
+        // -------------------------------------------------------------
+        webView.addJavascriptInterface(WebviewBridge(), "yeskeyAndroid")
+
+        // -------------------------------------------------------------
+        // UI없는 웹뷰 설정을 위한 Visibility (Optional)
+        // -------------------------------------------------------------
+
+        // -------------------------------------------------------------
+        // UI없는 웹뷰 설정을 위한 Visibility (Optional)
+        // -------------------------------------------------------------
+        webView.visibility = View.GONE
+
+        // -------------------------------------------------------------
+        // 기본 설정
+        // -------------------------------------------------------------
+
+        // -------------------------------------------------------------
+        // 기본 설정
+        // -------------------------------------------------------------
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadWithOverviewMode = true
+
+        // -------------------------------------------------------------
+        // 웹뷰 화면 설정 (Optional)
+        // -------------------------------------------------------------
+
+        // -------------------------------------------------------------
+        // 웹뷰 화면 설정 (Optional)
+        // -------------------------------------------------------------
+        settings.textZoom = 100
+        settings.setSupportMultipleWindows(true)
+        settings.javaScriptCanOpenWindowsAutomatically = true
+
+        // -------------------------------------------------------------
+        // 성능관련 최적화 (Optional)
+        // -------------------------------------------------------------
+
+        // -------------------------------------------------------------
+        // 성능관련 최적화 (Optional)
+        // -------------------------------------------------------------
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O) {
+            settings.setRenderPriority(WebSettings.RenderPriority.HIGH)
+            settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+            settings.setEnableSmoothTransition(true)
+        }
+        settings.setAppCacheEnabled(true)
     }
+
 
     // 초기화 parameter (Json String)
     var _paramStr: String? = genSdkInitParam()
@@ -45,14 +136,15 @@ class MainActivity : AppCompatActivity() {
      * @return pramString
      */
     private fun genSdkInitParam(): String? {
-        var param = LinkedHashMap<String, String>()
+        var param = LinkedHashMap<String, String?>()
         param["orgCode"] = "0099"
         param["apiKey"] = "a121b24e58d348fb123a912c3"
         param["clientOrigin"] = this.packageName
         param["uniqVal"] = getUniqueValue()
         param["lang"] = "kor"
         try {
-            return jsonMapper.writeValueAsString(param)
+            val mapper = ObjectMapper()
+            return mapper.writeValueAsString(param)
         } catch (e: java.lang.Exception) {
             Log.d("LOG_TAG", "SDK 초기화 파라미터 생성 실패 ")
         }
@@ -91,4 +183,14 @@ class MainActivity : AppCompatActivity() {
         return null
     }
 
+    class YesKeyWebView : WebViewClient() {
+        override fun onPageFinished(view: WebView, url: String) {
+            // sdkLoad
+            loadSdk()
+            // sdkInit
+            initSdk()
+            super.onPageFinished(view, url)
+        }
+    }
 }
+
